7db5ced624df57df1d431d2277fe02280df198c3
iamhelpingstar <150225912+iamhelpingstar@users.noreply.github.com>
2023-12-17 03:17:40 +0900

    Update copied_text_editor.py

--- copied_text_editor.py
+++ copied_text_editor.py
@@ -1,20 +1,41 @@
+import re
+from collections import deque
+
 path = "copied_text.md"
-answer_list = []
+answer_q = deque()
 is_answer = False
-with open(path, "r") as file:
+
+
+def contains_korean(text):
+    korean_regex = re.compile(r"[가-힣]")
+    return bool(korean_regex.search(text))
+
+
+with open(path, "r", encoding="UTF-8") as file:
     for line in file:
-        if line == 'ChatGPT\n':
+        if line == "ChatGPT\n":
             is_answer = True
             continue
-        elif line == 'User\n':
+        elif line == "User\n":
             is_answer = False
             continue
-        elif line == '\n':
+        elif len(line) < 10:
+            continue
+        elif line == "\n":
             continue
         if is_answer:
-            answer_list.append(line)
+            answer_q.append(line)
 
-with open(path, "w") as file:
-    for sen in answer_list:
-        file.write(sen)
-        file.write('\n')
+answer_dict = {}
+
+while answer_q:
+    eng = answer_q.popleft()
+    kor = answer_q.popleft()
+    answer_dict[eng] = kor
+
+with open("after" + path, "w", encoding="UTF-8") as file:
+    for eng, kor in answer_dict.items():
+        file.write(eng)
+        file.write("\n")
+        file.write(kor)
+        file.write("\n")
